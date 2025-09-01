91ef7e3527a3f53e0c3a17e3a45ef7cab2f6b850
Julian Leucker <leuckerj@gmail.com>
2025-09-01 18:19:10 +0200

    :sparkles: Link to heloise

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 import "iconify-icon";
 import '@fontsource/host-grotesk';
 import '@fontsource/playfair-display';
+
+document.querySelectorAll('.icon-container').forEach(container => {
+    const src = container.getAttribute("data-src");
+    fetch(src)
+        .then(res => res.text())
+        .then(svgText => {
+            container.innerHTML = svgText;
+            // Optional: remove width/height attributes to let CSS bestimmen
+            const svg = container.querySelector("svg");
+            svg.removeAttribute("width");
+            svg.removeAttribute("height");
+        })
+        .catch(err => console.error("SVG load error:", err));
+});
